348336158ffe7ca826717ee8808fc19e6a34d56e
Myusuf31 <myusufpolije31@gmail.com>
2023-11-08 13:43:44 +0700

    first commit

---
+++ src/javabank/InterfaceBankAccount.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package javabank;
+
+
+public interface InterfaceBankAccount {
+    public final String BANK= "JavaBank";
+public void deposit(int amt);
+public void withdraw(int amt);
+public int getBalance();
+public String getBankName();
+}
